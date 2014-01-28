1c6966fe79d23691bbfe27c75aaba9a33ca6e5e1
Patrick Steiger <psteiger@gmail.com>
2014-01-28 05:31:06 -0300

    adicionado aparelho. GUI previa. removido *.class. revisao de codigo

---
+++ Implementacao/MinhaCasa/src/Controlador/Aparelho/Aparelho.java
@@ -0,0 +1,37 @@
+package Controlador.Aparelho;
+
+import java.util.List;
+
+import Controlador.Aparelho.Aparelho;
+
+public class Aparelho {
+	private String nome;
+	private boolean on;
+	
+	public Aparelho(String aNome) {
+		this.nome = aNome;
+	}
+	
+	public String getNome()
+	{
+		return this.nome;
+	}
+
+	public boolean getStatus()
+	{
+		return on;
+	}
+	
+	public void ligaDesliga()
+	{
+		on = !on;
+	}
+	
+	public boolean putNome(String aNome)
+	{
+		this.nome = aNome;
+		if (!this.nome.equals(aNome))
+			return false;
+		return true;
+	}
+}
